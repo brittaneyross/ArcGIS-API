c9872c243d94b06e41ffc7c5a4a1090d6e447bb8
Brittaney <brittaney.e.ross@gmail.com>
2019-10-19 18:12:32 -0500

    Added update feature

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
-var view;
+
+var updateEdit = false
+//Style with renderers
+//renderers and symbols are needed for each type of data (point, line, polygon ect)
+//Symbology
+var defaultSym = {
+  type: 'simple-marker', //autocasts as a new SimpleFillSymbol() - you do not need to add a requirement arguement
+  style: 'square',
+  color: 'green',
+  outline: {
+    color: 'grey',
+    width: .25
+  }
+};
+
+//Renderer
+var renderer = {
+  type: 'simple', //autocasts as new SimpleRenderer()
+  symbol: defaultSym, //reference symbology set above
+  label: 'ndd points',
+  visualVariables: [{
+    type: 'color',
+    field: 'status_type', //set field to determine Symbology
+    stops: [{
+        value: 'complete',
+        color: 'green',
+        style: 'square',
+      },
+      {
+        value: 'in progress',
+        color: 'red',
+        style: 'square',
+      }
+    ]
+  }]
+}
 
 //Require statements passes in esri map and map view
 //Also passing in function calls Map and MapView
@@ -11,29 +46,228 @@ var view;
 // set properties
 // add it to your Map
 
-require(["esri/Map",
-  "esri/views/MapView",
-  "esri/layers/FeatureLayer",
-  "dojo/domReady!"
-], function(Map, MapView, FeatureLayer) {
-  //the names of the functoions can be named anything
-  //Instantiat the Map
-  //Create the new map
-  //the map is the container of the map
-  var map = new Map({
-    //set basemap to one of the available online
-    basemap: "streets"
+require(['esri/Map',
+'esri/views/MapView',
+'esri/layers/FeatureLayer',
+'esri/widgets/Popup',
+'esri/PopupTemplate',
+'esri/widgets/Editor',
+'esri/popup/content/AttachmentsContent',
+'esri/popup/content/TextContent',
+'dojo/domReady!'
+], function(Map,
+MapView,
+FeatureLayer,
+Popup,
+PopupTemplate,
+Editor,
+AttachmentsContent,
+TextContent) {
+//the names of the functions can be named anything
+
+//editing
+let editor, features;
+
+let attachmentsElement = new AttachmentsContent({
+  displayType: "list"
+});
+
+//Create new editor constructor
+const editThisAction = {
+  title: 'Edit feature',
+  id: 'edit-this',
+  className: 'esri-icon-edit'
+};
+
+//PopupTemplate
+var PopupTemplate = new PopupTemplate({
+  title: '<h3>Northeastern Illinois Development View</h3>',
+  content: [
+    {
+      type: 'text',
+      text:'<hr>' +
+        '<h2>Building Name - Building Street Address</h2> ' +
+        '<h4>icon | Full building address</h4>' +
+        '<h4>icon | link to datahub or cmap ndd</h4>' +
+        '<hr>' +
+        '<h4>Development Status</h4> - {status_type}'+
+        '<h4>Contact</h4> - {contact}'+
+        '<hr>'
+    },
+    {
+      type: 'attachments'
+    }
+  ],
+  fieldInfos:[
+    {
+      fieldName: 'status_type'
+    },
+    {
+      fieldName: 'contact'
+    }
+  ],
+  actions: [editThisAction],
+})
+
+
+//instantiate feature layer
+var cmapCounties = new FeatureLayer({
+  url: 'http://services5.arcgis.com/LcMXE3TFhi1BSaCY/arcgis/rest/services/MPOcounties_CMAP_201409/FeatureServer'
+})
+
+var nddPoints = new FeatureLayer({
+  url: 'https://services5.arcgis.com/GwzfxPSYbDxtMoCu/arcgis/rest/services/points/FeatureServer',
+  outFields: ['*'],
+  opacity: 0.8,
+  renderer: renderer,
+  popupTemplate: PopupTemplate
+})
+
+//Instantiate the Map
+//Create the new map
+//the map is the container of the map
+var map = new Map({
+  //set basemap to one of the available online
+  basemap: 'streets'
+});
+//view is how you view the Map
+//multiple view can reference the same map but in a different location/extent
+//Map is like the backyard and the view of the map is like the window from your house
+//each window can have a different view of the same map
+var view = new MapView({
+  //all properties set are directly in constructor in 4x
+  //all properties can be set when instantiating the map view
+  container: 'viewDiv',
+  map: map,
+  zoom: 9,
+  center: [-88.1285691261062, 41.793264502709576],
+  popup: {
+    dockEnabled: true,
+    dockOptions: {
+      buttonEnabled: false,
+      breakpoint: false,
+      position: 'bottom-left',
+    }
+  },
+
+});
+
+editor = new Editor({
+  view: view,
+  allowedWorkflows:['create']
+});
+
+view.ui.add(editor, "bottom-right");
+
+//add feature layer to Map using when statement
+//to load layer when map is done loading
+view.when(function() {
+  map.add(cmapCounties);
+  map.add(nddPoints);
+
+  editor = new Editor({
+    view: view,
+    container: document.createElement('div'),
+    layerInfos: [{
+      layer: nddPoints,
+      fieldConfig: [{
+          name: 'status_type',
+          label: 'status_type',
+          hint: 'Select status from dropdown'
+        },
+        {
+          name: 'contact',
+          label: 'contact',
+          hint: 'Enter contact info'
+        }
+      ]
+    }]
+  })
+
+  // Execute each time the 'Edit feature' action is clicked
+  function editThis() {
+    updateEdit = true
+    // If the EditorViewModel's activeWorkflow is null, make the popup not visible
+    if (!editor.viewModel.activeWorkFlow) {
+      view.popup.visible = false;
+      // Call the Editor update feature edit workflow
+
+      editor.startUpdateWorkflowAtFeatureEdit(
+        view.popup.selectedFeature
+      );
+      view.ui.add(editor, 'top-right');
+      view.popup.spinnerEnabled = false;
+    }
+
+    // We need to set a timeout to ensure the editor widget is fully rendered. We
+    // then grab it from the DOM stack
+    setTimeout(function() {
+      // Use the editor's back button as a way to cancel out of editing
+      let arrComp = editor.domNode.getElementsByClassName(
+        'esri-editor__back-button esri-interactive'
+      );
+      if (arrComp.length === 1) {
+        // Add a tooltip for the back button
+        arrComp[0].setAttribute(
+          'title',
+          'Cancel edits, return to popup'
+        );
+        // Add a listerner to listen for when the editor's back button is clicked
+        arrComp[0].addEventListener('click', function(evt) {
+          // Prevent the default behavior for the back button and instead remove the editor and reopen the popup
+          evt.preventDefault();
+          view.ui.remove(editor);
+          view.popup.open({
+            features: features
+          });
+        });
+      }
+    }, 150);
+  }
+
+  // Event handler that fires each time an action is clicked
+  view.popup.on('trigger-action', function(event) {
+    if (event.action.id === 'edit-this') {
+      editThis();
+    }
   });
-  //view is how you view the Map
-  //multiple view can reference the same map but in a different location/extent
-  //Map is like the backyard and the view of the map is like the window from your house
-  //each window can have a different view of the same map
-  view = new MapView({
-    //all properties set are directly off of constructor in 4x
-    //all properties can be set when instantiating the map view
-    container: "viewDiv",
-    map: map,
-    zoom: 9,
-    center: [-88.1285691261062,41.793264502709576]
-  });
+});
+
+// Watch when the popup is visible
+view.popup.watch('visible', function(event) {
+  // Check the Editor's viewModel state, if it is currently open and editing existing features, disable popups
+  if (editor.viewModel.state === 'editing-existing-feature') {
+    view.popup.close();
+  } else {
+    // Grab the features of the popup
+    features = view.popup.features;
+  }
+});
+
+nddPoints.on('apply-edits', function() {
+  if(updateEdit == true){
+    // Once edits are applied to the layer, remove the Editor from the UI
+    view.ui.remove(editor);
+
+    // Iterate through the features
+    features.forEach(function(feature) {
+      // Reset the template for the feature if it was edited
+      feature.popupTemplate = PopupTemplate;
+    });
+
+    // Open the popup again and reset its content after updates were made on the feature
+    if (features) {
+      view.popup.open({
+        features: features
+      });
+    }
+
+    // Cancel the workflow so that once edits are applied, a new popup can be displayed
+    editor.viewModel.cancelWorkflow();
+
+    updateEdit = false
+  }
+
+});
+
 });
